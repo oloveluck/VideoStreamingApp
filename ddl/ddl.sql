1113d23c17bab1f722fc23735eb1964fff899611
Hismooth <71531577+Hismooth@users.noreply.github.com>
2020-11-12 13:43:45 -0500

    Add files via upload

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -1,35 +1,30 @@
 DROP database project;
 CREATE database project;
-CREATE TABLE user (
+CREATE TABLE Users (
 	FirstName VARCHAR(255), 
 	LastName VARCHAR(255), 
 	Email VARCHAR(255) PRIMARY KEY,
 	Password VARCHAR(255), 
 	Country VARCHAR(255)
 );
-
-CREATE TABLE video (
+CREATE TABLE Video (
 	ID INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	ReleaseDate DATE, 
 	Title VARCHAR(255), 
 	Description VARCHAR(255)
 );
-
-CREATE TABLE app (
+CREATE TABLE App (
 	Name VARCHAR(255) PRIMARY KEY, 
 	Description VARCHAR(255)
 );
-
-CREATE TABLE show (
+CREATE TABLE Shows (
 	Name VARCHAR(255) PRIMARY KEY,
 	Description VARCHAR(255)
 );
-
-CREATE TABLE platform (
+CREATE TABLE Platform (
 	Name VARCHAR(255) PRIMARY KEY, 
 	IsMobile TINYINT(1)
 );
-
 CREATE TABLE PlatformApp (
 	Platform VARCHAR(255),
 	FOREIGN KEY(Platform) REFERENCES Platform(Name), 
@@ -39,10 +34,9 @@ CREATE TABLE PlatformApp (
 	Rating DOUBLE, 
 	Version DOUBLE
 );
-
 CREATE TABLE Season (
-	Show VARCHAR(255), 
-	FOREIGN KEY(Shows) REFERENCES Shows(Name), 
+	Shows VARCHAR(255), 
+	FOREIGN KEY(Shows) REFERENCES Shows(Name),
 	Video INT(8) UNSIGNED,
     PRIMARY KEY(Shows, Video),
 	FOREIGN KEY(Video) REFERENCES Video(ID), 
@@ -54,7 +48,7 @@ CREATE TABLE VideoApp (
 	FOREIGN KEY(Video) REFERENCES Video(ID), 
 	App VARCHAR(255),
     PRIMARY KEY(Video, App),
-	FOREIGN KEY(App) REFERENCES App(Name), 
+    FOREIGN KEY(App) REFERENCES App(Name), 
 	Subscription TINYINT(1)
 );
 
@@ -68,7 +62,7 @@ CREATE TABLE UserLikes (
 
 CREATE TABLE WantsToWatch (
 	User VARCHAR(255),
-	FOREIGN KEY(User) REFERENCES Users(Email), 
+	FOREIGN KEY(User) REFERENCES Users(Email),
 	Video INT(8) UNSIGNED,
     PRIMARY KEY(Video, User),
 	FOREIGN KEY(Video) REFERENCES Video(ID)
@@ -76,8 +70,8 @@ CREATE TABLE WantsToWatch (
 
 CREATE TABLE UserWatched (
 	User VARCHAR(255),
-	FOREIGN KEY(User) REFERENCES Users(Email), 
+	FOREIGN KEY(User) REFERENCES Users(Email),
 	Video INT(8) UNSIGNED,
     PRIMARY KEY(Video, User),
 	FOREIGN KEY(Video) REFERENCES Video(ID)
-);
+); 
